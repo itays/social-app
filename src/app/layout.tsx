d23daedaaa1bdaedfa7c1ea65ea6871bbefc0fdb
Itay Sidis <itay@linxsecurity.io>
2024-08-10 10:52:01 +0300

    added Post model. Toaster provider in layout.tsx. A PostEditor component and createPOst schema validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter, Roboto_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -38,6 +39,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
