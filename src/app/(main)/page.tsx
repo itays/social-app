a1629088241d059533d71f6e4d6d9e08d731df70
Itay Sidis <itaysidis@gmail.com>
2024-08-10 12:50:14 +0300

    creating posts. setting up tiptap editor

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,10 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <main className="w-full">main page</main>;
+  return (
+    <main className="w-full">
+      <PostEditor />
+    </main>
+  );
 }
